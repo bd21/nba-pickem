92367a55e58cbb8bebf72395cd0869e58de7a295
bdenniston <bdenniston@expediagroup.com>
2020-12-25 16:11:02 -0800

    fix

--- components/BetCard.tsx
+++ components/BetCard.tsx
@@ -10,12 +10,11 @@ import { ListItem, Avatar } from 'react-native-elements'
 
 const list = [
   {
-    name: 'Amy Farha',
-    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-    subtitle: 'Vice President'
+    team_1: 'Lakers',
+    team_2: 'Celtics'
   },
   {
-    name: 'Chris Jackson',
+    team1: 'Kings',
     avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
     subtitle: 'Vice Chairman'
   },
